e991ce632e6cfdd2762ab5f21bda964dce2f8a16
Aaron ("AJ") Steers <aj@meltano.com>
2023-04-11 16:14:03 -0700

    fix: stream name

--- tap_beautifulsoup/streams.py
+++ tap_beautifulsoup/streams.py
@@ -8,14 +8,8 @@ from singer_sdk import typing as th  # JSON Schema typing helpers
 
 from tap_beautifulsoup.client import BeautifulSoupStream
 
-# TODO: Delete this is if not using json files for schema definition
-SCHEMAS_DIR = Path(__file__).parent / Path("./schemas")
-# TODO: - Override `UsersStream` and `GroupsStream` with your own stream definition.
-#       - Copy-paste as many times as needed to create multiple stream types.
-
 
 class GenericBSStream(BeautifulSoupStream):
     """Define custom stream."""
 
-    name = "users"
-    primary_keys = ["id"]
+    name = "page_content"
